a7e3109dc465fd48d91c0eebe7bc0b921b8cdce6
vijay <vijaysingh.ece90@gmail.com>
2025-07-12 01:23:07 +0530

    4th-commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "rg-1" {
   name     = "example"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "rg-3" {
-  name     = "example-3"
+resource "azurerm_resource_group" "rg-2" {
+  name     = "example-2"
   location = "West Europe"
 }
